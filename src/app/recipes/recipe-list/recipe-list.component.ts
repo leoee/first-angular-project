73e51efba57979f8080306fd14119edc4da495ee
Leonardo Eufrazio <leonardo.eufrazio@inatel.br>
2019-11-06 15:24:22 -0300

    Added ingredient model; Updated home-page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test',
+      'https://discovery-latam-prod.imgix.net/files/inline-images/TIPS_ARROZ_MEDIDA.JPG')
+  ];
   constructor() { }
 
   ngOnInit() {
