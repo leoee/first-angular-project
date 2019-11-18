32cc54c6f181fa63d804a9312cda7a27dc7721da
Leonardo Nogueira <leonardo.eufrazio@inatel.br>
2019-11-18 15:10:22 -0300

    Add dropdown menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'first-angular-project';
   loadedFeature = 'recipe';
-  
+
   onNavigate(feature: string) {
     this.loadedFeature = feature;
   }
--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,10 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
-
+  dropRef = false;
   constructor() { }
 
   ngOnInit() {
   }
-
 }
